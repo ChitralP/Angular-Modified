6402340f3a2492ed7e19948e386e401bb477825c
unknown <chitralpatil@gmail.com>
2020-04-22 15:44:27 +0530

    Angular Component Part 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar'; 
 import { FlexLayoutModule } from '@angular/flex-layout';  
 import { MatListModule } from '@angular/material/list';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+
 
 import { AppComponent } from './app.component';
 
@@ -21,6 +25,9 @@ import { MenuComponent } from './menu/menu.component';
     MatToolbarModule,
     FlexLayoutModule,
     MatListModule,
+    MatGridListModule,
+    MatCardModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,5 +1,48 @@
 import { Component, OnInit } from '@angular/core';
 import { Dish } from '../shared/dish';
+const DISHES: Dish[] = [
+  {
+    id: '0',
+    name: 'Vpizza ',
+    image: '/assets/images/uthappizza.png',
+    category: 'mains',
+    featured: true,
+    label: 'Hot',
+    price: '4.99',
+    // tslint:disable-next-line:max-line-length
+    description: 'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with stale Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.'
+  },
+  {
+    id: '1',
+    name: 'Zucchiconstipation',
+    image: '/assets/images/zucchipakoda.png',
+    category: 'appetizer',
+    featured: false,
+    label: '',
+    price: '1.99',
+    description: 'Deep fried Zucchini coated with mildly spiced Chickpea flour batter accompanied with a sweet-tangy tamarind sauce or is it something else?'
+  },
+  {
+    id: '2',
+    name: 'Cholesterol',
+    image: '/assets/images/vadonut.png',
+    category: 'appetizer',
+    featured: false,
+    label: 'New',
+    price: '1.99',
+    description: 'A quintessential Vlounge experience, is it a vada or is it a donut?'
+  },
+  {
+    id: '3',
+    name: 'Diarrhoea',
+    image: '/assets/images/elaicheesecake.png',
+    category: 'dessert',
+    featured: false,
+    label: '',
+    price: '2.99',
+    description: 'A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms'
+  }
+ ];
 
 @Component({
   selector: 'app-menu',
@@ -8,49 +51,8 @@ import { Dish } from '../shared/dish';
 })
 export class MenuComponent implements OnInit {
 
-  dishes: Dish[] = [
-    {
-      id: '0',
-      name: 'Vpizza ',
-      image: '/assets/images/uthappizza.png',
-      category: 'mains',
-      featured: true,
-      label: 'Hot',
-      price: '4.99',
-      // tslint:disable-next-line:max-line-length
-      description: 'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with stale Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.'
-    },
-    {
-      id: '1',
-      name: 'Zucchiconstipation',
-      image: '/assets/images/zucchipakoda.png',
-      category: 'appetizer',
-      featured: false,
-      label: '',
-      price: '1.99',
-      description: 'Deep fried Zucchini coated with mildly spiced Chickpea flour batter accompanied with a sweet-tangy tamarind sauce or is it something else?'
-    },
-    {
-      id: '2',
-      name: 'Cholesterol',
-      image: '/assets/images/vadonut.png',
-      category: 'appetizer',
-      featured: false,
-      label: 'New',
-      price: '1.99',
-      description: 'A quintessential Vlounge experience, is it a vada or is it a donut?'
-    },
-    {
-      id: '3',
-      name: 'Diarrhoea',
-      image: '/assets/images/elaicheesecake.png',
-      category: 'dessert',
-      featured: false,
-      label: '',
-      price: '2.99',
-      description: 'A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms'
-    }
-   ];
+  dishes = DISHES
+  selectedDish = DISHES[0];
 
   constructor() { }
 
