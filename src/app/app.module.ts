221b4aa3d4ee3df54a150fabac3558e84d4a3b62
unknown <chitralpatil@gmail.com>
2020-04-27 18:59:16 +0530

    Angular Router Basics

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,10 +14,15 @@ import { AppComponent } from './app.component';
 import 'hammerjs';
 import { MenuComponent } from './menu/menu.component';
 import { DishDetailComponent } from './dish-detail/dish-detail.component';
-
-import { DishService } from './services/dish.service';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+
+import { DishService } from './services/dish.service';
+import { AppRoutingModule } from './app-routing/app-routing.module';
+
 
 
 @NgModule({
@@ -26,7 +31,10 @@ import { FooterComponent } from './footer/footer.component';
     MenuComponent,
     DishDetailComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
@@ -36,11 +44,12 @@ import { FooterComponent } from './footer/footer.component';
     MatListModule,
     MatGridListModule,
     MatCardModule,
-    MatButtonModule
+    MatButtonModule,
+    AppRoutingModule
   ],
   providers: [
-    DishService
-  ],
+    DishService,
+    ],
   bootstrap: [AppComponent],
  
 })
