217568e493a394d875cfbf58bbc61527c4d2b1cb
unknown <chitralpatil@gmail.com>
2020-04-27 18:36:23 +0530

    Header & Footer added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,13 +16,17 @@ import { MenuComponent } from './menu/menu.component';
 import { DishDetailComponent } from './dish-detail/dish-detail.component';
 
 import { DishService } from './services/dish.service';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    DishDetailComponent
+    DishDetailComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
